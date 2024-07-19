0fbd28e230c8275c0f555fa960a02fda131d2ee7
nisam <nisammt@gmail.com>
2024-07-19 23:03:20 +0530

    project added

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+ function AddTwonums()
+ {
+    
+  let a = parseInt(document.getElementById('firstnumber').value);
+  let b = parseInt(document.getElementById('secondnumber').value);
+  let sum = a + b;
+  document.getElementById('result').value=sum;
+         
+ }
